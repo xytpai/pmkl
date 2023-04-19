0f03987f09b6d12a59f520aab635fad28ebd85d5
xytpai <xytpai@foxmail.com>
2023-04-19 07:28:31 +0000

    introduce pmkl/core

---
+++ pmkl/core/core.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "launcher.h"
+#include "tensor.h"
+#include "tensor_iterator.h"
+#include "tensor_loops.h"
+#include "measure.h"
+#include "fill.h"
--- pmkl/pmkl.h
+++ pmkl/pmkl.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include "launcher.h"
-#include "tensor.h"
-#include "tensor_iterator.h"
-#include "tensor_loops.h"
-#include "measure.h"
-#include "fill.h"
+#include "core.h"
 
 namespace pmkl {
 
